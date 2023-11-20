6d5f1a78c1bbd9ed17d2fdbef809f5527f4768dc
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2023-11-20 13:27:07 +0100

    Primer commit en Eclipse :P

---
+++ SaludosEDD/src/holaMundo/SaludosEntornoDesarrollo.java
@@ -0,0 +1,10 @@
+package holaMundo;
+
+public class SaludosEntornoDesarrollo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola a todos los alumnos de Entornos de desarrollo");
+	}
+
+}
