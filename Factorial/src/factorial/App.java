f8ad92c42f609c334013e304151e8b41e1784248
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2023-11-24 12:33:38 +0100

    Cambios factortiales

---
+++ Factorial/src/factorial/App.java
@@ -0,0 +1,19 @@
+package factorial;
+
+public class App {
+
+	public static void main(String[]args) {
+		System.out.println("El factorial de 5 es " + factorial(5));
+	}
+
+
+	public static int factorial(int n) {
+		int resultado=1;
+		for (int i = n; i >= 1; i--) {
+			resultado = resultado*i;
+		}
+		return resultado;
+	}
+
+}
+
