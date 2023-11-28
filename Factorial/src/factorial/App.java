ce8784c56372fed99b53bae5cf68bc66f24b947f
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2023-11-28 13:37:21 +0100

    Todo lo nuevo añadido

--- Factorial/src/factorial/App.java
+++ Factorial/src/factorial/App.java
@@ -2,18 +2,11 @@ package factorial;
 
 public class App {
 
-	public static void main(String[]args) {
-		System.out.println("El factorial de 5 es " + factorial(5));
-	}
-
-
-	public static int factorial(int n) {
-		int resultado=1;
-		for (int i = n; i >= 1; i--) {
-			resultado = resultado*i;
+	public static void main(String[] args) {
+		MultiFactorial mf = new MultiFactorial(150);
+		mf.imprimeFactorial();
 		}
-		return resultado;
+			
+		
 	}
-
-}
 
